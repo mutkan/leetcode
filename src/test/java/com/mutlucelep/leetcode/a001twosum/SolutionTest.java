eac7d0384ed46c6813e9d7d59e1c2b7b96ca13f1
mutlu <mutkanx@gmail.com>
2019-07-05 16:02:13 -0500

    First commit

---
+++ src/test/java/com/mutlucelep/leetcode/a001twosum/SolutionTest.java
@@ -0,0 +1,26 @@
+package com.mutlucelep.leetcode.a001twosum;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+
+public class SolutionTest {
+  private Solution solution;
+
+  @BeforeMethod
+  public void setUp() throws Exception {
+    this.solution = new Solution();
+  }
+
+  @Test
+  public void testTwoSum() throws Exception {
+    int[] nums = {2, 7, 11, 15};
+    int target = 9;
+
+    int[] expected = {0, 1};
+
+    assertEquals(this.solution.twoSum(nums, target), expected);
+  }
+}
