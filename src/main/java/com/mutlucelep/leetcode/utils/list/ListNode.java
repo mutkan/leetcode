770783ea96efbaf3b89c93b40ed9efdb6bedd14b
mutlu <mutkanx@gmail.com>
2019-07-10 12:49:06 -0500

    Optimize the List Node

--- src/main/java/com/mutlucelep/leetcode/utils/list/ListNode.java
+++ src/main/java/com/mutlucelep/leetcode/utils/list/ListNode.java
@@ -8,20 +8,4 @@ public class ListNode {
         this.val = val;
         next = null;
     }
-
-    public int getVal(){
-        return val;
-    }
-
-    public void setVal(int val){
-        this.val = val;
-    }
-
-    public ListNode getNext(){
-        return next;
-    }
-
-    public void setNext(ListNode next){
-        this.next = next;
-    }
 }
