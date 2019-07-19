6cbe9979fc96442a2ef70c4ec1a49ff9d7e99f1f
mutlu <mutkanx@gmail.com>
2019-07-19 13:23:38 -0500

    Add test for first unique character problem

---
+++ src/test/java/com/mutlucelep/leetcode/a387firstuniquecharacterinastring/SolutionTest.java
@@ -0,0 +1,38 @@
+package com.mutlucelep.leetcode.a387firstuniquecharacterinastring;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class SolutionTest {
+    private Solution solution = new Solution();
+
+    @Test
+    public void testFirstUniqueChar_1() {
+        String s = "leetcode";
+        int expected = 0;
+        int actual = solution.firstUniqChar(s);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testFirstUniqueChar_2() {
+        String s = "loveleetcode";
+        int expected = 2;
+        int actual = solution.firstUniqChar(s);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testFirstUniqueChar_3() {
+        String s = "lll";
+        int expected = -1;
+        int actual = solution.firstUniqChar(s);
+
+        assertEquals(actual, expected);
+    }
+
+
+}
