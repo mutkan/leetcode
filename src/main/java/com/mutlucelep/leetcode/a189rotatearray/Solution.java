1751db1d4d79ea030697dc84491eaa4d0ae49482
mutlu <mutkanx@gmail.com>
2019-08-14 23:33:20 -0500

    Add solution for rotate array problem

---
+++ src/main/java/com/mutlucelep/leetcode/a189rotatearray/Solution.java
@@ -0,0 +1,58 @@
+package com.mutlucelep.leetcode.a189rotatearray;
+
+/**
+ * URL:
+ * =====
+ * https://leetcode.com/problems/rotate-array/
+ * <p>
+ * Desc:
+ * =====
+ * Given an array, rotate the array to the right by k steps, where k is non-negative.
+ * <p>
+ * Example 1:
+ * <p>
+ * <p>
+ * Input: [1,2,3,4,5,6,7] and k = 3 ,4321 765 5671234  7654321
+ * Output: [5,6,7,1,2,3,4]
+ * Explanation:
+ * rotate 1 steps to the right: [7,1,2,3,4,5,6]
+ * rotate 2 steps to the right: [6,7,1,2,3,4,5]
+ * rotate 3 steps to the right: [5,6,7,1,2,3,4]
+ * <p>
+ * Example 2:
+ * <p>
+ * <p>
+ * Input: [-1,-100,3,99] and k = 2
+ * Output: [3,99,-1,-100]
+ * Explanation:
+ * rotate 1 steps to the right: [99,-1,-100,3]
+ * rotate 2 steps to the right: [3,99,-1,-100]
+ * <p>
+ * Note:
+ * <p>
+ * <p>
+ * Try to come up as many solutions as you can, there are at least 3 different ways to solve this problem.
+ * Could you do it in-place with O(1) extra space?
+ */
+
+class Solution {
+    public void rotate(int[] nums, int k) {
+        k = k % nums.length;
+        reverse(nums,0, nums.length-k-1);
+        reverse(nums, nums.length-k,nums.length-1);
+        reverse(nums, 0,nums.length-1);
+    }
+
+    void reverse(int[] nums, int start, int end){
+        int temp;
+
+        while(start<end){
+            temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+}
